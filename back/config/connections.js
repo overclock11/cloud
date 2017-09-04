8b652062b6817e3ba93493453acf7670c5f78ab3
fredygonzalocaptuayonovoa <fredygonzalocaptuayonovoa@192.168.0.5>
2017-09-03 21:12:48 -0500

    se avanza con admin

--- back/config/connections.js
+++ back/config/connections.js
@@ -36,7 +36,7 @@ module.exports.connections = {
       host      : 'localhost',
       port      : 3306,
       user      : 'root',
-      password  : 'admin',
+      password  : 'root',
       database  : 'cloud'
   }
 
