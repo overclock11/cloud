62623be06a96b31d956b144f525e3b23a6669774
overclock11 <jfborrayg@libertadores.edu.co>
2017-09-24 13:12:59 -0500

    se agrega variable endpointWorker para la configuracion del despliegue b

--- front/src/app/services/cargar-videos.service.ts
+++ front/src/app/services/cargar-videos.service.ts
@@ -5,13 +5,13 @@ import configuracion from '../app.config';
 
 @Injectable()
 export class CargarVideosService {
-  public base:string = "http://"+configuracion.endpoint+":3000/api/videos/";
+  public base:string = "http://"+configuracion.endpointWorker+":3000/api/videos/";
   constructor(private http:Http) { }
 
 
   uploadVideo(datos:FormData): Observable<Response>{
     let headers = {'Content-Type': 'multipart/form-data'};
-    return this.http.post("http://"+configuracion.endpoint+":3001/api/videos/upload",datos,headers)
+    return this.http.post("http://"+configuracion.endpointWorker+":3001/api/videos/upload",datos,headers)
     .catch((err)=>Observable.throw("Algo salio mal",err));
   }
   registrarVideo(participante:any): Observable<Response>{
