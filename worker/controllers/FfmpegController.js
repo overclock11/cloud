5ab69fbd36a937eb51b45703a7d53184f05ffcf7
EC2 Default User <ec2-user@ip-172-31-33-99.us-west-2.compute.internal>
2017-09-24 02:58:35 +0000

    ajuste11

--- worker/controllers/FfmpegController.js
+++ worker/controllers/FfmpegController.js
@@ -5,8 +5,33 @@ var config = require('../config');
 // ffmpeg convert function
 exports.convertVideoToMp4 = function(id, urlOrigin, emailUser){
 
+if (urlOrigin.indexOf(".mp4") > 0) {
+
 console.log("va a convertir el video"+urlOrigin);
 	try {
+		var process = new ffmpeg(config.pathVideo.pathLogicOrigin+urlOrigin);
+		process.then(function (video) {
+      		video
+			.save(config.pathVideo.pathLogicConvert+urlOrigin, function (error, file) {
+        console.log(error);
+			  if (error){
+          console.log(error);
+        }
+        else {
+          console.log('Video file: ' + file);
+  				console.log("Video a convertido");
+  				EmailController.sendEmail(id, emailUser, config.pathVideo.pathRender+urlOrigin);
+        }
+			});
+			}, function (err) {
+				console.log('Error: ' + err);
+			});
+		} catch (e) {
+			console.log(e.code);
+			console.log(e.msg);
+		}
+}  else {
+try {
 		var process = new ffmpeg(config.pathVideo.pathLogicOrigin+urlOrigin);
 		process.then(function (video) {
       urlOrigin = urlOrigin.replace('.avi', '.mp4');
@@ -33,5 +58,7 @@ console.log("va a convertir el video"+urlOrigin);
 			console.log(e.code);
 			console.log(e.msg);
 		}
+		}
 
 };
+
