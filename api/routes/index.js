dc91991ed88b8e432aee6fff077a0d2117525c2f
Julian Borray <jlian92@hotmail.com>
2017-10-03 17:11:29 -0500

    Se organizan rutas de mongo y mysql

--- api/routes/index.js
+++ api/routes/index.js
@@ -11,24 +11,34 @@ router.get('/', function(req, res)
   res.render('index', { title: 'Servicio rest con nodejs, express 4 y mysql'});
 });
 
-router.route("/usuario/crear").post(UserController.crear);
+/**
+ * Rutas mongo despliegue D
+ */
+router.route("/usuario/mcrear").post(UserController.mcrear);
+
+router.route("/competition/murl/:id").get(CompetitionController.murl);
+router.route("/competition/competitions/madmin/:id").get(CompetitionController.mgetAllCompetitionsAdmin);
+router.route("/competition/mcompetitions").get(CompetitionController.mgetAllCompetitionsHome);
+router.route("/competition/mid/:id").get(CompetitionController.mgetCompetitionsById);
+router.route("/competition/mupdate/:id").put(CompetitionController.mupdateCompetition);
+router.route("/competition/mcrear").post(CompetitionController.mregisterCompetition);
+router.route("/competition/meliminar/:id").delete(CompetitionController.mdeleteCompetition);
+
+
+/**
+ * Rutas Mysql para el despliegue C
+ */
+
 router.route("/usuario/login").post(UserController.login);
 
-
 router.route("/competition/url/:id").get(CompetitionController.url);
-router.route("/competition/murl/:id").get(CompetitionController.murl);
 router.route("/competition/competitions/admin/:id").get(CompetitionController.getAllCompetitionsAdmin);
-router.route("/competition/competitions/madmin/:id").get(CompetitionController.mgetAllCompetitionsAdmin);
 router.route("/competition/competitions").get(CompetitionController.getAllCompetitionsHome);
-router.route("/competition/mcompetitions").get(CompetitionController.mgetAllCompetitionsHome);
 router.route("/competition/id/:id").get(CompetitionController.getCompetitionsById);
-router.route("/competition/mid/:id").get(CompetitionController.mgetCompetitionsById);
 router.route("/competition/update/:id").put(CompetitionController.updateCompetition);
-router.route("/competition/mupdate/:id").put(CompetitionController.mupdateCompetition);
 router.route("/competition/crear").post(CompetitionController.registerCompetition);
-router.route("/competition/mcrear").post(CompetitionController.mregisterCompetition);
 router.route("/competition/eliminar/:id").delete(CompetitionController.deleteCompetition);
-router.route("/competition/meliminar/:id").delete(CompetitionController.mdeleteCompetition);
+
 
 
 router.route("/videos/competition/:id").get(VideoController.getVideoByCompetition);
