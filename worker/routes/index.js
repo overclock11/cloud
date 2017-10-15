5778dad34004849e5d790fcabc52f923f6cd316c
Fredy Gonzalo Captuayo Novoa <fredygonzalocaptuayonovoa@MacBook-Pro-de-Fredy.local>
2017-10-15 12:30:12 -0500

    se ajsuta nombre

--- worker/routes/index.js
+++ worker/routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var VideoController = require('../controllers/VideoController');
-
 
 /* Mostramos el formualario para crear usuarios nuevos */
 router.get('/', function(req, res)
@@ -9,5 +7,4 @@ router.get('/', function(req, res)
   res.render('index', { title: 'Servicio rest con nodejs, express 4 y mysql WORKER CON SEMAFORO 3002 v6'});
 });
 
-router.use('/api', router);
 module.exports = router;
